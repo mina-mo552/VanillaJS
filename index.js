be8f8918bf23711fc29f0629f0b44a64ad9ccfe4
mina-mo552 <momoian00@gmail.com>
2020-01-29 23:13:32 +0900

    theory
    .

---
+++ index.js
@@ -0,0 +1,37 @@
+/* const title = document.querySelector("#title");
+title.innerHTML = "Hi! Form JS";
+title.style.color = "red";
+document.title = "I own you now"; */
+
+/* const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init(); */
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+  s;
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
