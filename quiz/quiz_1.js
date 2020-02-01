0bbecc7e800e3c47b52dcfcc3cbb866f0f77cd2a
mina-mo552 <momoian00@gmail.com>
2020-02-01 19:57:31 +0900

    localStorage
    .

--- quiz/quiz_1.js
+++ quiz/quiz_1.js
@@ -1,4 +1,4 @@
-onst h2 = document.querySelector("h2");
+const h2 = document.querySelector("h2");
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
 const superEventHandler = {
